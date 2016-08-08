093b04195e594a056ab07b7a57d2a87def2fe523
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-08-08 20:10:25 +0300

    Preserve directories in Tcl86/
    v0.3.0

--- build.ps1
+++ build.ps1
@@ -1,10 +1,29 @@
-Function Make-PrefixedList([string]$prefix, [string[]]$list)
+Function Join-Lines
 {
-    "  $prefix """ + [system.String]::Join("""`n  $prefix """, $list) + """"
+    [system.String]::Join("`n", @($input))
 }
 
-$files = Get-ChildItem Tcl86 -Recurse -File | Resolve-Path -Relative | % { "$_" }
-$file_list = Make-PrefixedList "File" $files
-$delete_list = Make-PrefixedList "Delete" $files
-(Get-Content installer.nsi.in).Replace('FILE_LIST', $file_list).Replace('DELETE_LIST', $delete_list) | Set-Content installer.nsi
+Function Reverse
+{
+    $arr = @($input)
+    [array]::Reverse($arr)
+    $arr
+}
+
+Function Make-RelativeToInstallDir
+{
+    @($input) | Resolve-Path -Relative | % { "$_".Replace(".\", "`$INSTDIR\") }
+}
+
+Set-Location Tcl86/
+$dirs = Get-ChildItem . -Recurse -Directory | Make-RelativeToInstallDir
+$files = Get-ChildItem . -Recurse -File | Make-RelativeToInstallDir
+Set-Location ../
+$dir_commands = $dirs | % { "  CreateDirectory ""$_""" } | Join-Lines
+$file_commands = $files | % { "  File ""/oname=$_"" ""$($_.Replace("`$INSTDIR", "Tcl86"))""" } | Join-Lines
+$delete_commands = $files | % { "  Delete ""$_""" } | Join-Lines
+$rmdir_commands = $dirs | Reverse | % { "  RMDir ""$_""" } | Join-Lines
+(Get-Content installer.nsi.in).
+    Replace('CREATE_LIST', $dir_commands + "`n" + $file_commands).
+    Replace('DELETE_LIST', $delete_commands + "`n" + $rmdir_commands) | Set-Content installer.nsi
 & 'C:\Program Files (x86)\NSIS\Bin\makensis.exe' installer.nsi

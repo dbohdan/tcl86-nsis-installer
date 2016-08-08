1f56f4ac0dcdb2859b8f9e3e2b1b76ecd72956db
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-08-08 21:08:10 +0300

    build.ps1: Code formatting

--- build.ps1
+++ build.ps1
@@ -10,20 +10,27 @@ Function Reverse
     $arr
 }
 
-Function Make-RelativeToInstallDir
+Function Make-RelativeToInstDir
 {
     @($input) | Resolve-Path -Relative | % { "$_".Replace(".\", "`$INSTDIR\") }
 }
 
 Set-Location Tcl86/
-$dirs = Get-ChildItem . -Recurse -Directory | Make-RelativeToInstallDir
-$files = Get-ChildItem . -Recurse -File | Make-RelativeToInstallDir
+$dirs = Get-ChildItem . -Recurse -Directory | Make-RelativeToInstDir
+$files = Get-ChildItem . -Recurse -File | Make-RelativeToInstDir
 Set-Location ../
+
 $dir_commands = $dirs | % { "  CreateDirectory ""$_""" } | Join-Lines
-$file_commands = $files | % { "  File ""/oname=$_"" ""$($_.Replace("`$INSTDIR", "Tcl86"))""" } | Join-Lines
+$file_commands = `
+    $files `
+    | % { "  File ""/oname=$_"" ""$($_.Replace("`$INSTDIR", "Tcl86"))""" } `
+    | Join-Lines
 $delete_commands = $files | % { "  Delete ""$_""" } | Join-Lines
 $rmdir_commands = $dirs | Reverse | % { "  RMDir ""$_""" } | Join-Lines
+
 (Get-Content installer.nsi.in).
     Replace('CREATE_LIST', $dir_commands + "`n" + $file_commands).
-    Replace('DELETE_LIST', $delete_commands + "`n" + $rmdir_commands) | Set-Content installer.nsi
+    Replace('DELETE_LIST', $delete_commands + "`n" + $rmdir_commands) `
+| Set-Content installer.nsi
+
 & 'C:\Program Files (x86)\NSIS\Bin\makensis.exe' installer.nsi
